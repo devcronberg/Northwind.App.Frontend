766965d9a2138b193a7c034a7104ce1dd6f67d29
Michell Cronberg <michell@cronberg.dk>
2026-01-12 22:02:50 +0100

    Fix service worker registration path and update asset URLs in manifest

--- js/components/app-header.js
+++ js/components/app-header.js
@@ -12,7 +12,7 @@ class AppHeader extends HTMLElement {
         this.innerHTML = `
       <nav class="ui inverted blue menu" role="navigation" aria-label="Main navigation">
         <div class="ui container">
-          <a href="/" class="header item" aria-label="Northwind Traders home">
+          <a href="index.html" class="header item" aria-label="Northwind Traders home">
             <i class="shield icon" aria-hidden="true"></i>
             Northwind Traders
           </a>
